f1b177991501ebf31cf5c66f7a14b04bbac62f92
kissrain <cjyang1123@gmail.com>
2017-08-10 23:44:13 +0800

    add codes

--- src/main/java/com/rainbow/netty/server/download/HttpStaticFileServerHandler.java
+++ src/main/java/com/rainbow/netty/server/download/HttpStaticFileServerHandler.java
@@ -8,20 +8,80 @@ import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.util.CharsetUtil;
+import io.netty.util.internal.SystemPropertyUtil;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Pattern;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
+import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private static final Pattern INSECURE_URI = Pattern.compile(".*[<>&\"].*");
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
         if (!request.decoderResult()
                     .isSuccess()) {
-
+            sendError(ctx, HttpResponseStatus.BAD_REQUEST);
+            return;
         }
 
+        if (request.method() != HttpMethod.GET) {
+            sendError(ctx, HttpResponseStatus.METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        final String uri = request.uri();
+        final String path = sanitizeUri(uri);
+        if (path == null) {
+            sendError(ctx, FORBIDDEN);
+            return;
+        }
+
+        File file = new File(path);
+        if (file.isHidden() || !file.exists()) {
+            sendError(ctx, NOT_FOUND);
+            return;
+        }
+
+
+
+
+    }
+
+    private static String sanitizeUri(String uri) {
+        // Decode the path
+        try {
+            uri = URLDecoder.decode(uri, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            throw new Error(e);
+        }
+
+        if (uri.isEmpty() || uri.charAt(0) != '/') {
+            return null;
+        }
+
+        // Convert file separators
+        uri = uri.replace('/', File.separatorChar);
+
+        // Simplistic dumb security check
+        // You will have to do something serious in the production environment
+        if (uri.contains(File.separator + '.') || uri.contains('.' + File.separator) || uri.charAt(0) == '.' || uri.charAt(uri.length() - 1) == '.'
+                || INSECURE_URI.matcher(uri)
+                               .matches()) {
+            return null;
+        }
+
+        // Convert to absolute path
+        return SystemPropertyUtil.get("user.dir") + File.separator + uri;
     }
 
     private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
@@ -30,6 +90,7 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
                 .set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
 
         // Close the connection as soon as the error message is sent
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+        ctx.writeAndFlush(response)
+           .addListener(ChannelFutureListener.CLOSE);
     }
 }
