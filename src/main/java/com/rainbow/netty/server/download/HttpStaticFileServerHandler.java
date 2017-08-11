67b9d9c1daaa684373694048fb7d9b13d50d15ba
kissrain <cjyang1123@gmail.com>
2017-08-11 18:00:28 +0800

    add sendListing and sendRedirect method

--- src/main/java/com/rainbow/netty/server/download/HttpStaticFileServerHandler.java
+++ src/main/java/com/rainbow/netty/server/download/HttpStaticFileServerHandler.java
@@ -1,6 +1,8 @@
 package com.rainbow.netty.server.download;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -19,7 +21,9 @@ import java.net.URLDecoder;
 import java.util.regex.Pattern;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
+import static io.netty.handler.codec.http.HttpResponseStatus.FOUND;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
@@ -51,8 +55,15 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
             return;
         }
 
-
-
+        if (file.isDirectory()) {
+            if (uri.endsWith("/")) {
+                sendListing(ctx, file, uri);
+            }
+            else {
+                sendRedirect(ctx, uri + '/');
+            }
+            return;
+        }
 
     }
 
@@ -92,5 +103,63 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
         // Close the connection as soon as the error message is sent
         ctx.writeAndFlush(response)
            .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static final Pattern ALLOWED_FILE_NAME = Pattern.compile("[^-\\._]?[^<>&\\\"]*");
+
+    private static void sendListing(ChannelHandlerContext ctx, File dir, String dirPath) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK);
+        response.headers()
+                .set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+
+        StringBuilder buf = new StringBuilder().append("<!DOCTYPE html>\r\n")
+                                               .append("<html><head><meta charset='utf-8' /><title>")
+                                               .append("Listing of: ")
+                                               .append(dirPath)
+                                               .append("</title></head><body>\r\n")
+
+                                               .append("<h3>Listing of: ")
+                                               .append(dirPath)
+                                               .append("</h3>\r\n")
+
+                                               .append("<ul>")
+                                               .append("<li><a href=\"../\">..</a></li>\r\n");
+
+        for (File f : dir.listFiles()) {
+            if (f.isHidden() || !f.canRead()) {
+                continue;
+            }
+
+            String name = f.getName();
+            if (!ALLOWED_FILE_NAME.matcher(name)
+                                  .matches()) {
+                continue;
+            }
+
+            buf.append("<li><a href=\"")
+               .append(name)
+               .append("\">")
+               .append(name)
+               .append("</a></li>\r\n");
+        }
+
+        buf.append("</ul></body></html>\r\n");
+
+        ByteBuf buffer = Unpooled.copiedBuffer(buf, CharsetUtil.UTF_8);
+        response.content()
+                .writeBytes(buffer); // why need release?
+        buffer.release();
+
+        // Close the connection as soon as the error message is sent
+        ctx.writeAndFlush(response)
+           .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void sendRedirect(ChannelHandlerContext ctx, String newUri) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, FOUND);
+        response.headers().set(HttpHeaderNames.LOCATION, newUri);
+
+        // Close the connection as soon as the error message is sent
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 }
