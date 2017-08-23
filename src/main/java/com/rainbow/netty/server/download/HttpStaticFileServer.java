1d1f78df9187ab815ef54001e5051618c3466d36
kissrain <cjyang1123@gmail.com>
2017-08-23 19:58:55 +0800

    modify err print

--- src/main/java/com/rainbow/netty/server/download/HttpStaticFileServer.java
+++ src/main/java/com/rainbow/netty/server/download/HttpStaticFileServer.java
@@ -44,7 +44,7 @@ public class HttpStaticFileServer {
             Channel ch = b.bind(PORT)
                           .sync()
                           .channel();
-            System.err.println("Open your web browser and navigate to " + (SSL ? "https" : "http") + "://127.0.0.1" + PORT + '/');
+            System.err.println("Open your web browser and navigate to " + (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
             ch.closeFuture()
               .sync();
         }
